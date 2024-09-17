dcf6df7bf0cc54e0cd9b4da4f00b2d99c6744b88
Moazam05 <salmanmoazam08@gmail.com>
2024-09-17 18:02:26 +0500

    warning close

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import AppNavigator from './src/AppNavigator';
+import {LogBox} from 'react-native';
+
+// Ignore specific warnings
+LogBox.ignoreLogs(['ReactImageView: Image source "null" doesn\'t exist']);
 
 const App = () => {
   return <AppNavigator />;
