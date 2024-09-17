cebf4f60f4a8c02afdbbe5d649e0691701552aeb
Moazam05 <salmanmoazam08@gmail.com>
2024-09-17 13:31:46 +0500

    icons import

---
+++ src/assets/images/index.js
@@ -0,0 +1,27 @@
+import Back from './back.png';
+import Cart from './cart.png';
+import HomeFill from './home_fill.png';
+import Home from './home.png';
+import MenuIcon from './menu.png';
+import NotificationFill from './notification_fill.png';
+import Notification from './notification.png';
+import SearchIcon from './search.png';
+import UserFill from './user_fill.png';
+import User from './user.png';
+import WishlistFill from './wishlist_fill.png';
+import Wishlist from './wishlist.png';
+
+export default {
+  Back,
+  Cart,
+  HomeFill,
+  Home,
+  MenuIcon,
+  NotificationFill,
+  Notification,
+  SearchIcon,
+  UserFill,
+  User,
+  WishlistFill,
+  Wishlist,
+};
