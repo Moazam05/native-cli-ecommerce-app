70c7a56dcd33daacac828d14c5042578b8ccf8d9
Moazam05 <salmanmoazam08@gmail.com>
2024-09-18 17:06:34 +0500

    ui issue fixed

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,12 +1,33 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Header from '../components/Header';
+import {Back, CartIcon} from '../assets/images';
+import {useNavigation} from '@react-navigation/native';
+import useTypedSelector from '../hooks/useTypedSelector';
+import {selectedProducts} from '../redux/products/productsSlice';
 
 const Cart = () => {
+  const navigation = useNavigation();
+  const cartProducts = useTypedSelector(selectedProducts);
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header
+        leftIcon={Back}
+        rightIcon={CartIcon}
+        title="Your Shopping Cart"
+        leftClick={() => navigation.goBack()}
+      />
+    </SafeAreaView>
   );
 };
 
 export default Cart;
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+};
