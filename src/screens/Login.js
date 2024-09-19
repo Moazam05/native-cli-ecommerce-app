be3931c8f04e213953bd5cc29cae4ef5577bb095
Moazam05 <salmanmoazam08@gmail.com>
2024-09-19 15:14:15 +0500

    login ui

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,16 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {LoginImg} from '../assets/images';
 
 const Login = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Login</Text>
+      <Image source={LoginImg} style={styles.coverImage} />
+
+      <Text style={styles.tagline}>
+        Welcome back! We're glad to see you again.
+      </Text>
     </SafeAreaView>
   );
 };
@@ -16,5 +21,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f5f5f5',
+  },
+  coverImage: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 2.5,
+    resizeMode: 'cover',
+  },
+  tagline: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginVertical: 20,
+    color: '#333',
+    fontWeight: '600',
   },
 });
