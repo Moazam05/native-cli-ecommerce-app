aafc420281f77597bc5a3bf600ef80e43463701c
Moazam05 <salmanmoazam08@gmail.com>
2024-09-26 17:40:12 +0500

    css refactor

--- src/screens/PreMain.js
+++ src/screens/PreMain.js
@@ -13,14 +13,15 @@ const PreMain = () => {
       <View style={styles.coverImg}>
         <GetStartedImg />
       </View>
-      <Text style={styles.tagLine}>You want Authentic, here you go!</Text>
-      <Text style={styles.findText}>Find it here, buy it now!</Text>
-
-      <View style={styles.buttonText}>
-        <CustomButton
-          name="Get Started"
-          onPress={() => navigation.navigate('Main')}
-        />
+      <View style={styles.content}>
+        <Text style={styles.tagLine}>You want Authentic, here you go!</Text>
+        <Text style={styles.findText}>Find it here, buy it now!</Text>
+        <View style={styles.buttonText}>
+          <CustomButton
+            name="Get Started"
+            onPress={() => navigation.navigate('Main')}
+          />
+        </View>
       </View>
     </View>
   );
@@ -29,15 +30,40 @@ const PreMain = () => {
 export default PreMain;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 30,
+  },
+  coverImg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    alignItems: 'center',
+    marginBottom: 35,
+  },
   tagLine: {
     fontSize: 34,
     color: Colors.WHITE,
     fontFamily: Fonts.SEMIBOLD,
+    textAlign: 'center',
+    marginBottom: 15,
   },
-
   findText: {
     fontSize: 14,
     color: '#F2F2F2',
     fontFamily: Fonts.REGULAR,
+    textAlign: 'center',
+    marginBottom: 45,
+  },
+  buttonText: {
+    width: '100%',
+    paddingHorizontal: 20,
   },
 });
