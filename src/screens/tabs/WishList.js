5d3c946a4de8791dc175502264275a3dc42c378c
Moazam05 <salmanmoazam08@gmail.com>
2024-09-28 23:01:19 +0500

    refactor

--- src/screens/tabs/WishList.js
+++ src/screens/tabs/WishList.js
@@ -20,23 +20,37 @@ import {
   selectWishlistProducts,
   setWishListProducts,
 } from '../../redux/wishlist/wishlistsSlice';
-import {MasonryFlashList} from '@shopify/flash-list';
 
 const WishList = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const wishListProducts = useTypedSelector(selectWishlistProducts);
-  console.log('wishListProducts', wishListProducts);
 
+  const randomHeight = () => {
+    return Math.floor(Math.random() * (180 - 120 + 1)) + 120;
+  };
   const removeFromWishlist = item => {
     dispatch(setWishListProducts(item));
   };
-
   const renderItem = ({item}) => (
-    <View style={styles.productContainer}>
+    <View style={[styles.productContainer, {height: randomHeight()}]}>
       <Image source={item?.image} style={styles.productImage} />
+      {/* Wishlist Icon */}
+      <TouchableOpacity
+        style={styles.wishlistIcon}
+        onPress={() => removeFromWishlist(item)}>
+        <Image
+          source={WishlistFill}
+          style={{
+            width: 18,
+            height: 18,
+            tintColor: '#F83758',
+          }}
+        />
+      </TouchableOpacity>
       <View style={styles.productInfo}>
         <Text style={styles.productTitle}>{item?.name}</Text>
+        <Text style={styles.productDescription}>{item?.description}</Text>
       </View>
     </View>
   );
@@ -58,8 +72,7 @@ const WishList = () => {
           renderItem={renderItem}
           keyExtractor={item => item.id.toString()}
           contentContainerStyle={styles.listContainer}
-          tab
-          height
+          // numColumns={2}
           ListEmptyComponent={
             <Text style={styles.emptyText}>No items in your wishlist</Text>
           }
@@ -83,5 +96,43 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  productContainer: {
+    flex: 1,
+    margin: 8,
+    borderRadius: 10,
+    overflow: 'hidden',
+    position: 'relative',
+  },
+  productImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  wishlistIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    backgroundColor: '#F9F9F9',
+    padding: 8,
+    borderRadius: 50,
+  },
+  productInfo: {
+    padding: 8,
+    backgroundColor: '#fff',
+  },
+  productTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  productDescription: {
+    fontSize: 14,
+    color: '#888',
+  },
+  emptyText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+    color: '#888',
   },
 });
