8d4f2b50511db28f63ad25c9f339fbd5a11aecdd
Moazam05 <salmanmoazam08@gmail.com>
2024-09-28 21:24:29 +0500

    wishlist refactor

--- src/screens/tabs/WishList.js
+++ src/screens/tabs/WishList.js
@@ -9,8 +9,12 @@ import {
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch} from 'react-redux';
-import Header from '../../components/Header';
-import {CartIcon, MenuIcon, WishlistFill} from '../../assets/images'; // Add HeartIcon if not available
+import {
+  Logo,
+  NavigationIcon,
+  UserIcon,
+  WishlistFill,
+} from '../../assets/images'; // Add HeartIcon if not available
 import useTypedSelector from '../../hooks/useTypedSelector';
 import {
   selectWishlistProducts,
@@ -21,46 +25,38 @@ const WishList = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const wishListProducts = useTypedSelector(selectWishlistProducts);
+  console.log('wishListProducts', wishListProducts);
 
   const removeFromWishlist = item => {
     dispatch(setWishListProducts(item));
   };
 
-  const renderItem = ({item}) => (
-    <></>
-    // <View style={styles.productContainer}>
-    //   <Image source={{uri: item.image}} style={styles.productImage} />
-    //   <View style={styles.productInfo}>
-    //     <Text style={styles.productTitle}>{item.title}</Text>
-    //     <View style={styles.priceWrap}>
-    //       <Text style={styles.productPrice}>${item.price.toFixed(2)}</Text>
-    //       <TouchableOpacity onPress={() => removeFromWishlist(item)}>
-    //         <Image source={WishlistFill} style={styles.wishlistIcon} />
-    //       </TouchableOpacity>
-    //     </View>
-    //   </View>
-    // </View>
-  );
+  const renderItem = ({item}) => <></>;
 
   return (
     <View style={styles.container}>
-      <Header
-        leftIcon={MenuIcon}
-        rightIcon={CartIcon}
-        title="Wishlist Items"
-        leftClick={() => navigation.openDrawer()}
-      />
-      <FlatList
-        data={wishListProducts}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-        contentContainerStyle={styles.listContainer}
-        tab
-        height
-        ListEmptyComponent={
-          <Text style={styles.emptyText}>No items in your wishlist</Text>
-        }
-      />
+      <View style={styles.parentWrap}>
+        {/* Top Bar */}
+        <View style={styles.topBar}>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <NavigationIcon />
+          </TouchableOpacity>
+          <Logo />
+          <UserIcon />
+        </View>
+
+        <FlatList
+          data={wishListProducts}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+          contentContainerStyle={styles.listContainer}
+          tab
+          height
+          ListEmptyComponent={
+            <Text style={styles.emptyText}>No items in your wishlist</Text>
+          }
+        />
+      </View>
     </View>
   );
 };
@@ -70,60 +66,14 @@ export default WishList;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
   },
-  listContainer: {
-    padding: 20,
-    paddingBottom: 80,
+  parentWrap: {
+    paddingHorizontal: 16,
   },
-  productContainer: {
+  topBar: {
+    paddingVertical: 12,
     flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 15,
-    marginBottom: 20,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    elevation: 5,
-  },
-  productImage: {
-    width: 60,
-    height: 80,
-    borderRadius: 10,
-    marginRight: 15,
-  },
-  productInfo: {
-    flex: 1,
-  },
-  priceWrap: {
-    flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'space-between',
-  },
-  productTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 5,
-    minHeight: 50,
-  },
-  productPrice: {
-    fontSize: 16,
-    fontWeight: '500',
-    color: '#0786DAFD',
-  },
-  wishlistIcon: {
-    width: 24,
-    height: 24,
-    tintColor: '#0786DAFD',
-  },
-  emptyText: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: '#666',
-    marginTop: 20,
+    alignItems: 'center',
   },
 });
