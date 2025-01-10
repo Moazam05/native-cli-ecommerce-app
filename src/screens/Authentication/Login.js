bd0f55754f498a0d9f07bea771a060ce20c88ba3
Moazam05 <salmanmoazam08@gmail.com>
2025-01-10 21:22:45 +0500

    bug removed

--- src/screens/Authentication/Login.js
+++ src/screens/Authentication/Login.js
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import firestore from '@react-native-firebase/firestore';
 import {useNavigation} from '@react-navigation/native';
 import {Formik} from 'formik';
 import React, {useState} from 'react';
