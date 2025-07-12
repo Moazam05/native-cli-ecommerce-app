c4254d4f7c0d39fe6bdf1bd43a1055e78951c983
Moazam05 <salmanmoazam08@gmail.com>
2025-07-12 15:05:02 +0500

    package name change for play store

--- android/app/src/main/java/com/ecommerce/MainActivity.kt
+++ android/app/src/main/java/com/ecommerce/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ecommerce
+package com.ecommerceMyApp
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/ecommerce/MainApplication.kt
+++ android/app/src/main/java/com/ecommerce/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ecommerce
+package com.ecommerceMyApp
 
 import android.app.Application
 import com.facebook.react.PackageList
